1f0f34f2d2f7b6c068cf751c84a81439529928da
Naoya Minomura <naotoitoi@gmail.com>
2023-11-28 23:12:21 +0900

    地域名表示追加

--- webmap/src/main.ts
+++ webmap/src/main.ts
@@ -1,5 +1,6 @@
-import { Map } from 'maplibre-gl';
+import { Map,Marker,Popup } from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css'
+import ota from '../data/ota.json';
 
 const map = new Map({
   container:'map',
@@ -13,6 +14,8 @@ const map = new Map({
         attribution: '&copy; OpenStreetMap contributors',
       },
     },
+    //glyphs
+    glyphs: "https://glyphs.geolonia.com/{fontstack}/{range}.pbf",
     layers:[
       {
         id: 'osm',
@@ -33,7 +36,7 @@ map.on('load', function () {
   });
   map.addSource('machi', {
     type: 'geojson',
-    data: './data/h27ka13111.json',
+    data: ota,
   });
   map.addLayer({
       id: 'ku',
@@ -60,6 +63,48 @@ map.on('load', function () {
         'line-color': '#000000',
         'line-width': 1,
     },
+  });
+  // ポリゴンの中心に地名を表示
+  ota.features.forEach((feature: any) => {
+    const coordinates = feature.geometry.coordinates[0].reduce(
+      (acc: [number, number], coord: [number, number]) => {
+        acc[0] += coord[0];
+        acc[1] += coord[1];
+        return acc;
+      },
+      [0, 0]
+    );
+    const center:[number,number] = [coordinates[0] / feature.geometry.coordinates[0].length, coordinates[1] / feature.geometry.coordinates[0].length];
+
+    map.addSource(`place-${feature.properties.S_NAME}`, {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {
+              name: feature.properties.S_NAME
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: center
+            }
+          }
+        ]
+      }
+    });
+
+    map.addLayer({
+      id: `place-label-${feature.properties.S_NAME}`,
+      type: 'symbol',
+      source: `place-${feature.properties.S_NAME}`,
+      layout: {
+        'text-field': feature.properties.S_NAME,
+        'text-font': ['Noto Sans CJK JP Regular'],
+      },
+    });
+
   });
 });
 
